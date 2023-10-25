dda14dde5769f84dd62c5f4fb20edd6eb955214c
one_punch_man <ckim@banlaw.com>
2023-10-25 11:12:22 +1100

    add section name

--- app/components/Advertise.jsx
+++ app/components/Advertise.jsx
@@ -32,18 +32,19 @@ const adList = [
 
 export default function AdvertiseSection() {
   return (
-    <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-      <div className="sm:border-primary-300 sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
-        {adList.map((advertise, index) => {
-          return (
-            <div
-              key={index}
-              className="flex flex-col items-center justify-center mx-4 my-4 sm:mx-0 sm:my-0"
-            >
-              <h2 className="text-white text-4xl font-bold flex flex-row">
-                {advertise.prefix}
-                {advertise.value}
-                {/* <AnimatedNumbers
+    <section className="text-white" id="advertise">
+      <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <div className="sm:border-primary-300 sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
+          {adList.map((advertise, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-col items-center justify-center mx-4 my-4 sm:mx-0 sm:my-0"
+              >
+                <h2 className="text-white text-4xl font-bold flex flex-row">
+                  {advertise.prefix}
+                  {advertise.value}
+                  {/* <AnimatedNumbers
                   includeComma
                   animateToNumber={parseInt(advertise.value)}
                   locale="en-US"
@@ -56,13 +57,14 @@ export default function AdvertiseSection() {
                     };
                   }}
                 /> */}
-                {advertise.postfix}
-              </h2>
-              <p className="text-[#ADB7BE] text-base">{advertise.metric}</p>
-            </div>
-          );
-        })}
+                  {advertise.postfix}
+                </h2>
+                <p className="text-[#ADB7BE] text-base">{advertise.metric}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
